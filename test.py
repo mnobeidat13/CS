b7a5c6fd6451f7ed3687c13e5a999a03ebcf073d
Mohammad Nur Aldeen Obeidat <32016587+mnobeidat13@users.noreply.github.com>
2024-01-16 10:33:32 +0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,46 @@
+# Count how many `Ahmad` in the list
+
+names = ['Ahmad', 'Zein', 'Ali', 'Ruba', 'Jad', 'Ahmad', 'Ali']
+
+count = 0
+
+for name in names:
+    if # Condition #:
+        count += 1
+    
+print(count)
+
+
+######################
+
+# check for each grade in the list
+# if the grade is less than `60` print `Fail`
+# if the grade is between `60` and `80` print `Average`
+# if the grade is greater than `80` print `Achiever`
+
+grades = [65, 80, 90, 56, 78, 87]
+
+for grade in grades:
+    if #Condition:
+        print('Fail')
+    elif #Condition:
+        print('Average')
+    else:
+        print('Achiever')
+
+
+students = {'Ahmad':80, 'Ali':75, 'Jad':90}
+
+# get the grade og `Ahmad`:
+
+#####
+
+# add a new name with a grade:
+
+#####
+
+# update an existing item
+
+#####
+        
+        
