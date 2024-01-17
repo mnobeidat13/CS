addd743fd547c86b7af3fd757b7c19be02d5b770
Mohammad Nur Aldeen Obeidat <32016587+mnobeidat13@users.noreply.github.com>
2024-01-17 07:48:39 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,20 @@
+# The hashtags are instructions, dont write them
+
 # Count how many `Ahmad` in the list
-
 names = ['Ahmad', 'Zein', 'Ali', 'Ruba', 'Jad', 'Ahmad', 'Ali']
-
 count = 0
-
 for name in names:
-    if # Condition #:
+    # Replace the #Condition with the correct code
+    if #Condition:
         count += 1
-    
 print(count)
-
-
 ######################
 
 # check for each grade in the list
 # if the grade is less than `60` print `Fail`
 # if the grade is between `60` and `80` print `Average`
 # if the grade is greater than `80` print `Achiever`
-
 grades = [65, 80, 90, 56, 78, 87]
-
 for grade in grades:
     if #Condition:
         print('Fail')
@@ -27,6 +22,13 @@ for grade in grades:
         print('Average')
     else:
         print('Achiever')
+
+
+
+
+
+
+
 
 
 students = {'Ahmad':80, 'Ali':75, 'Jad':90}
